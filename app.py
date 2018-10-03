67b5876247646d2a0fa7d89da55023d0ee23e4f5
Charley Ferrari <charley@charleyferrari.com>
2018-10-03 17:11:04 -0400

    app is working, config is raw

--- app.py
+++ app.py
@@ -8,6 +8,24 @@ import config
 from auth import auth
 from utils import StaticUrlPath
 
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+import plotly.graph_objs as go
+
+gapminder = pd.read_csv(
+    'https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+gapminder['logGdpPercap'] = np.log(gapminder['gdpPercap'])
+gapminder['logPop'] = np.log(gapminder['pop'])
+gapminder['text'] = gapminder.apply(lambda x:
+                                    'Year: {}<br>Country: {}<br>GDP Per Capita: {}<br>Life Expectancy: {}'.format(
+                                        np.round(x['year'], 2),
+                                        x['country'],
+                                        np.round(x['gdpPercap'], 2),
+                                        np.round(x['lifeExp'], 2)
+                                    ), axis=1)
 
 app = dash.Dash(
     __name__,
@@ -25,8 +43,11 @@ app.layout = html.Div(className='container', children=[
         Column(width=4, children=[
             dcc.Dropdown(
                 id='dropdown',
-                options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-                value='LA'
+                options=[
+                    {'label': 'GDP Per Capita', 'value': 'logGdpPercap'},
+                    {'label': 'Population', 'value': 'logPop'}
+                ],
+                value='logGdpPercap'
             )
         ]),
         Column(width=8, children=[
@@ -35,24 +56,45 @@ app.layout = html.Div(className='container', children=[
     ])
 ])
 
+
 @app.callback(Output('graph', 'figure'),
               [Input('dropdown', 'value')])
-def update_graph(value):
-    return {
-        'data': [{
-            'x': [1, 2, 3, 4, 5, 6],
-            'y': [3, 1, 2, 3, 5, 6]
-        }],
-        'layout': {
-            'title': value,
-            'margin': {
-                'l': 60,
-                'r': 10,
-                't': 40,
-                'b': 60
-            }
-        }
-    }
+def create_figure(column):
+    if column == 'logPop':
+        axis_name = 'Log Population'
+    else:
+        axis_name = 'Log GDP Per Capita'
+
+    lm = LinearRegression()
+
+    lm.fit(gapminder[column].reshape(-1, 1), gapminder['lifeExp'])
+
+    gapminder['predict'] = lm.predict(gapminder[column].reshape(-1, 1))
+    gapminder.sort_values('predict', inplace=True)
+
+    data = [
+        go.Scatter(
+            x=gapminder[column],
+            y=gapminder['lifeExp'],
+            mode='markers',
+            name='Data',
+            text=gapminder['text']
+        ),
+        go.Scatter(
+            x=gapminder[column],
+            y=gapminder['predict'],
+            mode='lines',
+            name='Best Fit'
+        )
+    ]
+
+    layout = go.Layout(
+        xaxis=dict(title=axis_name),
+        yaxis=dict(title='Life Expectancy')
+    )
+
+    return go.Figure(data=data, layout=layout)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
