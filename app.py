ccad2b977d05bc4ee27783d68e5ef3a71973716f
Charley Ferrari <charley@charleyferrari.com>
2018-11-05 14:45:18 -0500

    finishing image

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import dash_html_components as html
 
 from components import Column, Header, Row
 
+from skimage import io, filters, measure
+from scipy import ndimage
+
 app = dash.Dash(
     __name__
 )
@@ -17,18 +20,28 @@ app.layout = html.Div(className='container', children=[
     Header('Sample App'),
 
     Row([
-        Column(width=4, children=[
-            dcc.Dropdown(
-                id='dropdown',
-                options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-                value='LA'
-            )
+        html.P(['Choose a photo']),
+        dcc.Dropdown(
+            id='dropdown',
+            options=[{'label': i, 'value': i} for i in ['Coins', 'Cells']],
+            value='Coins'
+        )
+    ]),
+
+    Row([
+        Column(width=6, children=[
+            html.Img(id='img', style={
+                'width': '80%'
+            })
         ]),
-        Column(width=8, children=[
-            dcc.Graph(id='graph')
+        Column(width=6, children=[
+            html.P(id='img-info')
         ])
-    ])
+    ], style={'margin-top': '5px'})
 ])
+
+
+<< << << < HEAD
 
 
 @app.callback(Output('graph', 'figure'),
@@ -49,6 +62,41 @@ def update_graph(value):
             }
         }
     }
+
+
+== == == =
+
+
+@app.callback(
+    Output('img', 'src'),
+    [Input('dropdown', 'value')]
+)
+def update_img(value):
+    if value == 'Coins':
+        return 'assets/15368545052359371.jpg'
+    else:
+        return 'assets/ba3g0.jpg'
+
+
+@app.callback(
+    Output('img-info', 'children'),
+    [Input('dropdown', 'value')]
+)
+def update_img_info(value):
+    if value == 'Coins':
+        img_path = 'assets/15368545052359371.jpg'
+    else:
+        img_path = 'assets/ba3g0.jpg'
+
+    im = io.imread(img_path, as_gray=True)
+    val = filters.threshold_otsu(im)
+    drops = ndimage.binary_fill_holes(im < val)
+    labels = measure.label(drops)
+
+    return 'Scikit-Image has counted {} items'.format(str(labels.max()))
+
+
+>>>>>> > finishing image
 
 if __name__ == '__main__':
     app.run_server(debug=True)
