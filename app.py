ae57c51712b6946464bcb78e020ac2836cecc80d
Charley Ferrari <charley@charleyferrari.com>
2019-01-08 18:48:34 +0000

    reverting to original app

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-import dash
+mport dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
 from components import Column, Header, Row
 from auth import auth
-import requests
-
-
-try:
-    r = requests.get('http://www.google.com')
-    divstr = r.text
-except Exception as e:
-    divstr = str(e)
 
 app = dash.Dash(
     __name__
@@ -30,8 +22,42 @@ server = app.server  # Expose the server variable for deployments
 
 # Standard Dash app code below
 app.layout = html.Div(className='container', children=[
-	html.Div([divstr])
+
+    Header('Sample App'),
+
+    Row([
+        Column(width=4, children=[
+            dcc.Dropdown(
+                id='dropdown',
+                options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+                value='LA'
+            )
+        ]),
+        Column(width=8, children=[
+            dcc.Graph(id='graph')
+        ])
+    ])
 ])
+
+
+@app.callback(Output('graph', 'figure'),
+              [Input('dropdown', 'value')])
+def update_graph(value):
+    return {
+        'data': [{
+            'x': [1, 2, 3, 4, 5, 6],
+            'y': [3, 1, 2, 3, 5, 6]
+        }],
+        'layout': {
+            'title': value,
+            'margin': {
+                'l': 60,
+                'r': 10,
+                't': 40,
+                'b': 60
+            }
+        }
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
