0982486fe6a96866a43d4dab9810494f78c90722
cldougl <cldougl@gmail.com>
2019-01-23 02:50:33 -0500

    updates for 3.0.0 applications

--- app.py
+++ app.py
@@ -4,19 +4,10 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from components import Column, Header, Row
-from auth import auth
 
 app = dash.Dash(
     __name__
 )
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Optionally display a log in screen.                                                                   #
-# If `REQUIRE_LOGIN = True` in `config.py`, then auth_instance allows you to programatically access the #
-# username of the currently logged in user.                                                             #
-# If `REQUIRE_LOGIN = False`, then no login screen will be displayed and `auth_instance` will be `None` #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-auth_instance = auth(app)
 
 server = app.server  # Expose the server variable for deployments
 
